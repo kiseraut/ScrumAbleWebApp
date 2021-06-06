dfd321564d7dbef3d9f3089360317f363eb8ccdc
kiseraut <kiseraut@gmail.com>
2021-06-05 23:02:49 -0400

    Add project files.

---
+++ ScrumAble/Areas/Identity/Data/ScrumAbleUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ScrumAble.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the ScrumAbleUser class
+    public class ScrumAbleUser : IdentityUser
+    {
+    }
+}
---
+++ ScrumAble/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ScrumAble.Areas.Identity.Data;
+using ScrumAble.Data;
+
+[assembly: HostingStartup(typeof(ScrumAble.Areas.Identity.IdentityHostingStartup))]
+namespace ScrumAble.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ScrumAbleContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ScrumAbleContextConnection")));
+
+                services.AddDefaultIdentity<ScrumAbleUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ScrumAbleContext>();
+            });
+        }
+    }
+}
